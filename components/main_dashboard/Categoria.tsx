9615a8065d3829f3bc373eed8c4257905af83971
Ali_trk <trk.0010.0011@gmail.com>
2026-05-22 23:41:26 -0400

    Categorias main-dashborard

--- components/main_dashboard/Categoria.tsx
+++ components/main_dashboard/Categoria.tsx
@@ -1,23 +1,38 @@
-import React from 'react'
-import "./Categoria.css"
-import Tema from './Tema'
+import React from "react";
+import "./Categoria.css";
+import Tema from "./Tema";
 
-export default function Categoria() {
+type TemaType = {
+  id: number;
+  imagen: string;
+  nombre: string;
+  descripcion: string;
+};
+
+type Props = {
+  id: number;
+  nombre: string;
+  temas: TemaType[];
+};
+
+export default function Categoria({ id, nombre, temas }: Props) {
   return (
-    <div className='categoria'>
-        <div className='bloque_categoria'>
-            <h2>CATEGORIA N#</h2>
-        </div>
-        <div className='bloque_temas'>
-            <Tema/>
-            <Tema/>
-            <Tema/>
-            <Tema/>
-            <Tema/>
-            <Tema/>
-            <Tema/>
-            <Tema/>
-        </div>
+    <div className="categoria">
+      <div className="bloque_categoria">
+        <h2>{nombre}</h2>
+      </div>
+
+      <div className="bloque_temas">
+        {temas.map((tema) => (
+          <Tema
+            key={tema.id}
+            id={tema.id}
+            imagen={tema.imagen}
+            nombre={tema.nombre}
+            descripcion={tema.descripcion}
+          />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
--- components/main_dashboard/Encabezado.tsx
+++ components/main_dashboard/Encabezado.tsx
@@ -1,29 +1,37 @@
-'use client'
+"use client";
 
 import React from "react";
 import Image from "next/image";
 import Barra_buscar from "./Barra_buscar";
-import "./Encabezado.css"
+import "./Encabezado.css";
 
+type Props = {
+  nombre: string;
+};
 
-export default function Encabezado() {
-    const dir = "/dashboard/rata.svg"
-    const dir2 = "/dashboard/perfil.svg"
+export default function Encabezado({ nombre }: Props) {
+  const dir = "/dashboard/rata.svg";
+  const dir2 = "/dashboard/perfil.svg";
+
   return (
     <div className="bloque">
-        <div className="sub_bloque">
-            <button className="boton_encabezado">
-                <Image className="imagen" src={dir} alt="" width={50} height={50} />
-            </button>
-            <h1 className="nombre_materia">NOMBRE MATERIA</h1>
-        </div>
-        <Barra_buscar/>
-        <div className="sub_bloque">
-            <h2>Invitado</h2>
-            <button className="boton_encabezado">
-                <Image className="imagen" src={dir2} alt="" width={40} height={40}/>
-            </button>
-        </div>
-    </div>  
+      <div className="sub_bloque">
+        <button className="boton_encabezado">
+          <Image className="imagen" src={dir} alt="" width={50} height={50} />
+        </button>
+
+        <h1 className="nombre_materia">{nombre}</h1>
+      </div>
+
+      <Barra_buscar />
+
+      <div className="sub_bloque">
+        <h2>Invitado</h2>
+
+        <button className="boton_encabezado">
+          <Image className="imagen" src={dir2} alt="" width={40} height={40} />
+        </button>
+      </div>
+    </div>
   );
 }
